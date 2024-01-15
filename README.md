1304b72334000785c9079a6b2723d62bc8d090e2
sabershahhoseini <sabershahhoseini@gmail.com>
2024-01-15 17:42:27 +0330

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # block-Tor-ips
 Block Tor exit nodes using ipset or iptables.
+
+I recommend using `ipset`. Because `iptables` has pretty bad performance for large IP pools (even checking 1000 IPs is hard for iptables)
+
+But [ipset](https://ipset.netfilter.org/) is desined to block large IP pools.
+
+## Variables
+
+There are a few variables which you may want to take a look at.
+Variables at `.env` are used to provide URL to fetch IPS, iptables chain name or ipset set name.
+
+## Usage
+
+To use this script you must have root privilages.
+
+For iptables mode, run:
+
+```
+block-tor.sh iptables
+```
+
+To use this script for ipset mode, run:
+
+```
+block-tor.sh ipset
+```
---
+++ block-tor.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source .env
+# Check if required packages are installed
+check_packages() {
+	REQUIRED_COMMANDS=("iprange" "curl" "ipset" "iptables")
+	for i in ${REQUIRED_COMMANDS[@]}; do
+		if ! [ -x "$(command -v $i)" ]; then
+		  echo "Error: $i is not installed." >&2
+		  EXIT=true
+		fi
+	done
+	if [[ $EXIT ]]; then
+		exit 1
+	fi
+}
+
+fetch_tor_ips() {
+	# Get list of tor exit nodes
+	echo "Getting new IP addresses"
+	curl -s $BLOCK_LIST_URL > /tmp/tor-ips
+	
+	# Create a variable of rules and pass it to iprange utility to merge related networks into a CIDR
+	export IPS=$(cat /tmp/tor-ips | grep -v ':' | iprange)
+}
+
+
+# If iptables argument is passed, clear everyhing and add new rules with new IPs
+iptables_block() {
+	iptables -D INPUT -j BLOCK_TOR
+	iptables -F $CHAIN
+	iptables -X $CHAIN
+	iptables -N $CHAIN
+	
+	for IP in $IPS; do
+		echo "Blocking $IP on chain $CHAIN - iptables"
+		iptables -A $CHAIN -s $IP -j DROP
+	done
+	iptables -I INPUT 1 -j BLOCK_TOR
+}
+
+ipset_block() {
+	iptables -D INPUT -m set --match-set $SET src -j DROP
+	sleep 0.2
+	ipset destroy $SET
+	ipset create $SET hash:net
+	for IP in $IPS; do
+		echo "Blocking $IP on set $SET iptables"
+		ipset add $SET $IP
+	done
+	iptables -I INPUT 1 -m set --match-set $SET src -j DROP
+}
+
+
+
+check_packages
+fetch_tor_ips
+
+if [[ $1 == "iptables" ]]; then
+	iptables_block
+elif [[ $1 == "ipset" ]]; then
+	ipset_block
+fi
